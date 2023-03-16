42abffb7fc15c8b9e349c408b4778d5b81f2ab02
lfgberg <lfgaberg@gmail.com>
2023-03-16 15:23:47 -0400

    start inclass 7

---
+++ in-class/L07/L07.R
@@ -0,0 +1,11 @@
+# Setup libraries and data
+library(car)
+library(foreign)
+library(leaps)
+
+ic7.data = read.csv("covid19_SRA365.csv", header = TRUE)
+
+# which variables best predict new_deaths_per_million
+bestsubsets = regsubsets(new_deaths_per_million ~ new_cases_per_million + people_vaccinated_per_hundred
+                         + people_fully_vaccinated_per_hundred + median_age + gdp_per_capita
+                         + cardiovasc_death_rate + diabetes_prevalence + life_expectancy)
