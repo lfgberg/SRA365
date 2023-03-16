510106b3d1884a517986b265ae0963f0bfccc54b
lfgberg <lfgaberg@gmail.com>
2023-03-16 15:30:30 -0400

    update

--- in-class/L07/L07.R
+++ in-class/L07/L07.R
@@ -5,7 +5,13 @@ library(leaps)
 
 ic7.data = read.csv("covid19_SRA365.csv", header = TRUE)
 
-# which variables best predict new_deaths_per_million
+# which variables best predict new_deaths_per_million?
+
+# generate all possible models
 bestsubsets = regsubsets(new_deaths_per_million ~ new_cases_per_million + people_vaccinated_per_hundred
                          + people_fully_vaccinated_per_hundred + median_age + gdp_per_capita
                          + cardiovasc_death_rate + diabetes_prevalence + life_expectancy)
+
+# find the best models with each # of predictors
+
+# make all the best models and compare them
